5bc9e9d1fc0656c72b72c9c19e355b72942cb434
tendence <yppstc@gmail.com>
2017-02-24 19:57:01 +0800

    for using in Ubuntu without C++11 feature

--- caffe_layers/pair_fast_loss_layer.cpp
+++ caffe_layers/pair_fast_loss_layer.cpp
@@ -8,6 +8,11 @@
 #include "caffe/layers/pair_fast_loss_layer.hpp"
 
 namespace caffe {
+	bool ppsort(const pair<float, pair<int, int> >& a, const pair<float, pair<int, int> >& b)
+	{
+		return a.first > b.first;
+	}
+
 	template <typename Dtype>
 	void PairFastLossLayer<Dtype>::LayerSetUp(
 		const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top)
@@ -41,6 +46,8 @@ namespace caffe {
 	template <typename Dtype>
 	void PairFastLossLayer<Dtype>::Forward_cpu(
 		const vector<Blob<Dtype>*> & bottom, const vector<Blob<Dtype>*> & top){
+		//max_dist_class.clear();
+		//min_dist_class.clear();
 		const int channels = bottom[0]->channels();
 		const int nums = bottom[0]->num();
 		/** compute the diff_matrix and dist_matrix **/
@@ -78,15 +85,15 @@ namespace caffe {
 		Dtype triplet_rank_precision(0.0);
 		Dtype margin = this->layer_param_.pair_fast_loss_param().margin();// alpha
 		Dtype hard_ratio = this->layer_param_.pair_fast_loss_param().hard_ratio();// alpha
-		Dtype only_pos = this->layer_param_.pair_fast_loss_param().only_pos();// alpha
+		Dtype use_pos = this->layer_param_.pair_fast_loss_param().use_pos();// alpha
 
 		const Dtype* bottom_label = bottom[1]->cpu_data();
 
 		/** use the map to record accordding to the label info **/
-		map<int, vector<int>> label_data_map;
+		map<int, vector<int> > label_data_map;
 		/** record the loss information into 2 vector, the pos pair loss and neg pair loss seperately **/
-		vector<pair<float, pair<int, int>>> hard_loss_pos;
-		vector<pair<float, pair<int, int>>> hard_loss_neg;
+		vector<pair<float, pair<int, int> > > hard_loss_pos;
+		vector<pair<float, pair<int, int> > > hard_loss_neg;
 
 		int max_label = 0;
 		for (int i = 0; i < bottom[0]->num(); i++) {
@@ -102,10 +109,20 @@ namespace caffe {
 				label_data_map[label_value] = tmp;
 			}
 		}
+
+		//record the max distance within each class data
+
+		// for (auto const &ent : label_data_map) {
+		// 	max_dist_class[ent.first] = Dtype(0);
+		// 	min_dist_class[ent.first] = Dtype(10000);
+		// }
+
 		/** calculate the triplet precision **/
-		for (auto const &ent1 : label_data_map)
+		//for (auto const &ent1 : label_data_map)
+		for (map<int, vector<int> >::iterator itr1=label_data_map.begin();itr1!=label_data_map.end();itr1++)
 		{
-			int cur_size = label_data_map[ent1.first].size();
+			//int cur_size = label_data_map[ent1.first].size();
+			int cur_size = label_data_map[itr1->first].size();
 			if (cur_size < 2)
 			{
 				continue;
@@ -115,16 +132,25 @@ namespace caffe {
 			{
 				for (int k = j + 1; k < cur_size; k++)
 				{
-					int anc = label_data_map[ent1.first][j];
-					int pos = label_data_map[ent1.first][k];
-					for (auto const &ent2 : label_data_map)
+					//int anc = label_data_map[ent1.first][j];
+					//int pos = label_data_map[ent1.first][k];
+					int anc = label_data_map[itr1->first][j];
+					int pos = label_data_map[itr1->first][k];
+					//max_dist_class[ent1.first] = max(max_dist_class[ent1.first], dist_matrix.cpu_data()[anc * nums + pos]);
+					//min_dist_class[ent1.first] = min(min_dist_class[ent1.first], dist_matrix.cpu_data()[anc * nums + pos]);
+
+					//for (auto const &ent2 : label_data_map)
+					for (map<int, vector<int> >::iterator itr2=label_data_map.begin();itr2!=label_data_map.end();itr2++)
 					{
-						if (ent1.first == ent2.first) continue;
-						int m_size = label_data_map[ent2.first].size();
+						//if (ent1.first == ent2.first) continue;
+						if (itr1->first == itr2->first) continue;
+						//int m_size = label_data_map[ent2.first].size();
+						int m_size = label_data_map[itr2->first].size();
 						for (int n = 0; n < m_size; n++)
 						{
 							triplet_count += Dtype(2);
-							int neg = label_data_map[ent2.first][n];
+							//int neg = label_data_map[ent2.first][n];
+							int neg = label_data_map[itr2->first][n];
 							if (dist_matrix.cpu_data()[anc * nums + neg] > dist_matrix.cpu_data()[anc * nums + pos])   correct_rank_count += Dtype(1);
 							if (dist_matrix.cpu_data()[pos * nums + neg] > dist_matrix.cpu_data()[pos * nums + anc])   correct_rank_count += Dtype(1);
 						}
@@ -134,16 +160,20 @@ namespace caffe {
 		}
 
 		/** loop all the possible pair-wise dataset and triplet dataset **/
-		for (auto const &ent1 : label_data_map)
+		//for (std::pair<int, vector<int> > const &ent1 : label_data_map)
+		for (map<int, vector<int> >::iterator itr1=label_data_map.begin();itr1!=label_data_map.end();itr1++)
 		{
-			int cur_size = label_data_map[ent1.first].size();
+			//int cur_size = label_data_map[ent1.first].size();
+			int cur_size = label_data_map[itr1->first].size();
 			/** compute the same class pair-wise data loss **/
 			for (int i = 0; i < cur_size - 1; i++)
 			{
 				for (int j = i + 1; j < cur_size; j++)
 				{
-					int pos_1 = label_data_map[ent1.first][i];
-					int pos_2 = label_data_map[ent1.first][j];
+					//int pos_1 = label_data_map[ent1.first][i];
+					//int pos_2 = label_data_map[ent1.first][j];
+					int pos_1 = label_data_map[itr1->first][i];
+					int pos_2 = label_data_map[itr1->first][j];
 					float loss_pos_pair = dist_matrix.cpu_data()[pos_1 * nums + pos_2];
 					//float loss_pos_pair = max(dist_matrix.cpu_data()[pos_1 * nums + pos_2] - min_dist_class[ent1.first], Dtype(0.0));
 					if (loss_pos_pair == 0) continue;
@@ -155,22 +185,27 @@ namespace caffe {
 				}
 			}
 			//if only use pos pairs we just skip all the neg pairs loss
-			if (only_pos != 1) {
+			if (use_pos != 1) {
 				/** compute the different class pair-wise data loss **/
 				for (int i = 0; i < cur_size; i++)
 				{
 					int index_candidate = -1;
-					for (auto const &ent2 : label_data_map)
+					//for (std::pair<int, vector<int> > const &ent2 : label_data_map)
+					for (map<int, vector<int> >::iterator itr2=label_data_map.begin();itr2!=label_data_map.end();itr2++)
 					{
 						//Dtype cur_margin = max(max_dist_class[ent1.first], max_dist_class[ent2.first]);
 						index_candidate++;
-						if (ent1.first == ent2.first) continue;
-						int neg_size = label_data_map[ent2.first].size();
+						//if (ent1.first == ent2.first) continue;
+						if (itr1->first == itr2->first) continue;
+						//int neg_size = label_data_map[ent2.first].size();
+						int neg_size = label_data_map[itr2->first].size();
 						for (int j = 0; j < neg_size; j++)
 						{
 							neg_pair_count += Dtype(1);
-							int pos = label_data_map[ent1.first][i];
-							int neg = label_data_map[ent2.first][j];
+							//int pos = label_data_map[ent1.first][i];
+							//int neg = label_data_map[ent2.first][j];
+							int pos = label_data_map[itr1->first][i];
+							int neg = label_data_map[itr2->first][j];
 							Dtype loss_pos_neg = std::max(margin - dist_matrix.cpu_data()[pos * nums + neg], Dtype(0.0));
 							float tmp_neg_pair = loss_pos_neg;
 							if (pos < neg)  hard_loss_neg.push_back(std::make_pair(tmp_neg_pair, std::make_pair(pos, neg)));
@@ -183,18 +218,16 @@ namespace caffe {
 		}
 		/** sort all the hard sample set **/
 		if (hard_ratio < 1) {
-			sort(hard_loss_pos.begin(), hard_loss_pos.end(), [](const pair<float, pair<int, int>>& a, const pair<float, pair<int, int>>& b)
-			{	return a.first > b.first; });
-			if (only_pos != 1) {
-				sort(hard_loss_neg.begin(), hard_loss_neg.end(), [](const pair<float, pair<int, int>>& a, const pair<float, pair<int, int>>& b)
-				{	return a.first > b.first; });
+			sort(hard_loss_pos.begin(), hard_loss_pos.end(), ppsort);
+			if (use_pos != 1) {
+				sort(hard_loss_neg.begin(), hard_loss_neg.end(), ppsort);
 			}
 		}
 
-		int pos_hard_cnt = pos_pair_count * hard_ratio;
+		int pos_hard_cnt = pos_pair_count * 1;
 		int neg_hard_cnt = neg_pair_count * hard_ratio;
 		for (int i = 0; i < pos_hard_cnt; i++)  hard_loss += hard_loss_pos[i].first;
-		if (only_pos != 1) {
+		if (use_pos != 1) {
 			for (int i = 0; i < neg_hard_cnt; i++)  hard_loss += hard_loss_neg[i].first;
 		}
 
@@ -205,7 +238,7 @@ namespace caffe {
 				pair_matrix.mutable_cpu_data()[hard_loss_pos[i].second.first * nums + hard_loss_pos[i].second.second] = Dtype(1);
 			}
 		}
-		if (only_pos != 1) {
+		if (use_pos != 1) {
 			for (int i = 0; i < neg_hard_cnt; i++) {
 				if (hard_loss_neg[i].first > 0) {
 					all_hard_cnt++;
